50035aa15f72e23979051b1d49231b4420a1acad
siva <siva10@email.com>
2024-01-19 19:10:07 -0600

    hello

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World is coming on the way be ready" );
+        System.out.println( "Hello World is coming on the way this weekend  be ready" );
     }
 }
